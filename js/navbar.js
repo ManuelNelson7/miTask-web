db9da5bb1f84319d5bbeef5f1616c3aec6689991
ManuelNelson7 <manuel.nelson7@hotmail.com>
2022-02-07 21:55:11 -0300

    commit 7/2

---
+++ js/navbar.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.getElementById('links-menu').classList.toggle('links-show')
+};
+
+function showButtons() {
+    document.getElementById('buttons-menu').classList.toggle('buttons-show')
+};
