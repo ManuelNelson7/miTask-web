d2dc013e1a40fcd3c8008a39e31c3c93358375e6
ManuelNelson7 <manuel.nelson7@hotmail.com>
2022-03-05 23:12:44 -0300

    fixing responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,11 @@
 function showMenu() {
-    document.getElementById('links-menu').classList.toggle('links-show')
+    document.getElementById('links-menu').classList.toggle('links-show');
+    document.getElementById('buttons-menu').classList.remove('buttons-show');
 };
 
 function showButtons() {
-    document.getElementById('buttons-menu').classList.toggle('buttons-show')
+    document.getElementById('buttons-menu').classList.toggle('buttons-show');
+    document.getElementById('links-menu').classList.remove('links-show')
 };
 
 const scrollTrigger = 80;
@@ -11,10 +13,6 @@ const header = document.querySelector('header');
 
 window.onscroll = () => {
     window.scrollY >= scrollTrigger ?
-    header.classList.add('scrolled') :
-    header.classList.remove('scrolled');
+        header.classList.add('scrolled') :
+        header.classList.remove('scrolled');
 };
-
-let a = 5
-console.log(a++);
-console.log(++a);
